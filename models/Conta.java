e0e2effeaa8c1ca89107ad9af9ae42133a4f9ec1
Eduardo Soares <eduardo.soares00@edu.pucrs.br>
2020-05-23 01:11:34 -0300

    Codificação das models.

--- models/Conta.java
+++ models/Conta.java
@@ -1,11 +1,56 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Conta {
 
-    double saldo = 0;
+    private int id;
+    private Date dataCriacao;
+    private Operador operador;
+    private double saldo;
+    private ArrayList<Movimentacao> movimentacoes;
 
-    public Conta(double saldo) {
-        this.saldo = saldo;
+    public Conta(int id, Date dataCriacao, Operador operador) {
+        this.saldo = 0;
+        this.id = id;
+        this.dataCriacao = dataCriacao;
+        this.operador = operador;
+        this.movimentacoes = new ArrayList<>();
     }
 
+    //Pensar na verificação.
+    public void adicionarMovimentacao(int numeroDoc, String descricao, double valor, String tipo){
+        Movimentacao movimentacao = new Movimentacao(numeroDoc, descricao, valor, tipo);
+        movimentacoes.add(movimentacao);
+    }
+
+    public ArrayList<Movimentacao> consultarMovimentos(Date dataInicial, Date dataFinal){return movimentacoes;}
+    public ArrayList<Movimentacao> consultarMovimentos(Operador operador){return movimentacoes;}
+    public ArrayList<Movimentacao> consultarMovimentos(String tipo){return movimentacoes;}
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }   
+
+    public int getId() {
+        return id;
+    }
+
+    public ArrayList<Movimentacao> getMovimentacoes() {
+        return movimentacoes;
+    }
+
+    public Operador getOperador() {
+        return operador;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + " - " + this.dataCriacao + " - " + this.operador.getNome() + " - " + this.saldo;
+    }
 }
