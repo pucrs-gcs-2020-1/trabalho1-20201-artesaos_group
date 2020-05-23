8e6cdea701524d78c11978211998248280a0233d
João Brentano <joaobrentano.br@gmail.com>
2020-05-23 01:00:31 -0300

    Controllers codificados

--- controllers/GerenciadorContas.java
+++ controllers/GerenciadorContas.java
@@ -1,5 +1,41 @@
 package controllers;
 
+import models.Conta;
+import models.Operador;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class GerenciadorContas {
     
+    ArrayList<Conta> contas = new ArrayList<>();
+
+    public GerenciadorContas() {
+
+    }
+
+    public ArrayList<Conta> getContas() {
+        return this.contas;
+    }
+
+    public void cadastrarConta(Operador operador, double saldo) {
+        //Gera um novo ID para o Operador
+        int id_novo = contas.size() + 1;
+        //Pega o horário atual
+        Date date = new Date(System.currentTimeMillis());
+        //Cria conta e adiciona        
+        Conta conta = new Conta(id_novo, date, operador, saldo);
+        contas.add(conta);
+    }
+
+    public boolean transferirFundos(Conta contaOrigem, Conta contaDestino, double valor, int numeroDoc, String descricao) {
+        if((contaOrigem.getSaldo - valor)>0) {
+            contaOrigem.adicionarMovimentacao(numeroDoc, descricao, -valor);
+            contaDestino.adicionarMovimentacao(numeroDoc, descricao, valor);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- controllers/GerenciadorOperadores.java
+++ controllers/GerenciadorOperadores.java
@@ -1,14 +1,29 @@
 package controllers;
 
 import models.Operador;
+
 import java.util.ArrayList;
 
 public class GerenciadorOperadores {
 
-    ArrayList operadores = new ArrayList<Operador>();
+    ArrayList<Operador> operadores = new ArrayList<>();
 
     public GerenciadorOperadores() {
 
+    }
+
+    public void cadastrarOperador(String nome, String iniciais) {
+        Operador operador = new Operador(nome, iniciais);
+        operadores.add(operador);
+    }
+
+    public Operador consultarOperador(String iniciais) {
+        for (Operador op:operadores) {
+            if(op.getIniciais()==iniciais) {
+                return op;
+            }
+        }
+        return null;
     }
 
     public ArrayList<Operador> getOperadores() {
